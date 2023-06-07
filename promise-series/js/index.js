6b36b348bf6fb97fc40ca939665976a168ba0e5e
DragonLJH <1183665002@qq.com>
2023-06-07 18:28:42 +0800

    then方法的实现

--- promise-series/js/index.js
+++ promise-series/js/index.js
@@ -4,9 +4,13 @@ const REJECTED = 'rejected'
 const state = Symbol('state')
 const value = Symbol('value')
 const changeState = Symbol('changeState')
+const callbacks = Symbol('callbacks')
 class MyPromise {
+
+
     [state] = PENDING;
     [value] = void 0;
+    [callbacks] = []
     constructor(executor) {
         const resolve = (data) => {
             this[changeState](FULFILLED, data)
@@ -21,10 +25,39 @@ class MyPromise {
         }
     }
 
+
+
     [changeState](cState, data) {
         if (this[state] !== PENDING) return
         this[state] = cState
         this[value] = data
+        this[callbacks].forEach((item) => {
+            if (this[state] == FULFILLED) item.onfulfilled(data)
+            if (this[state] == REJECTED) item.onrejected(data)
+        })
+    }
+    then(onfulfilled, onrejected) {
+        return new MyPromise((resolve, reject) => {
+            const callback = {
+                onfulfilled: (data) => resolve(onfulfilled(data)),
+                onrejected: (reason) => reject(onrejected(reason)),
+            }
+
+            if (Object.prototype.toString.call(onfulfilled) == "[object Function]") {
+                if (this[state] == FULFILLED) {
+                    return resolve(onfulfilled(this[value]))
+                }
+            }
+            if (Object.prototype.toString.call(onrejected) == "[object Function]") {
+                if (this[state] == REJECTED) {
+                    return reject(onrejected(this[value]))
+                }
+            }
+            if (this[state] == PENDING) {
+                this[callbacks].push(callback)
+            }
+        })
+
     }
 }
 
@@ -33,16 +66,22 @@ class MyPromise {
 
 
 // executor: (resolve: (value: any) => void, reject: (reason?: any) => void) => void
+// then(onfulfilled?: ((value: any) => any) | null | undefined, onrejected?: ((reason: any) => PromiseLike<never>) | null | undefined): Promise<any>
 const p = new Promise((resolve, reject) => {
     setTimeout(() => {
-        throw 123
-    })
+            resolve(1)
+        }, 3000)
+        // resolve(1)
 })
+console.log(p.then((value) => {
+    return "pThen:" + value
+}))
 const p1 = new MyPromise((resolve, reject) => {
     setTimeout(() => {
-        throw 123
-    })
-
+            resolve(1)
+        }, 3000)
+        // resolve(1)
 })
-console.log(p)
-console.log(p1)
+console.log(p1.then((value) => {
+    return "p1Then:" + value
+}))
